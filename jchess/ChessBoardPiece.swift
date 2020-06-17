6961bcea08da6e33ae74ab25fd7ca99782ed949e
Patrick Hayes <patrickghhayes@gmail.com>
2020-06-17 12:58:31 -0700

    refactor code for getting image names out of the view

--- jchess/ChessBoardPiece.swift
+++ jchess/ChessBoardPiece.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ChessBoardPiece: View {
     var piece : Character
-    var pieceImage : String {getPieceImage(piece: piece)}
+    var pieceImage : String {ChessTile.getChessTileImage(piece: piece)}
     var id: String
     
     var body: some View {
@@ -20,39 +20,6 @@ struct ChessBoardPiece: View {
             .padding(.all, 5)
             .accessibility(label: Text(pieceImage))
             .accessibility(identifier: "chess piece " + id)
-    }
-    
-    func getPieceImage(piece: Character) -> String {
-        switch(piece) {
-            case "p":
-                return "black p"
-            case "P":
-                return "red p"
-            case "r":
-                return "black r"
-            case "R":
-                return "red r"
-            case "n":
-                return "black n"
-            case "N":
-                return "red n"
-            case "b":
-                return "black b"
-            case "B":
-                return "red b"
-            case "k":
-                return "black k"
-            case "K":
-                return "red k"
-            case "q":
-                return "black q"
-            case "Q":
-                return "red q"
-            case " ":
-                return "transparent"
-            default:
-                return "pawn"
-        }
     }
 }
 
--- jchess/Models/ChessTile.swift
+++ jchess/Models/ChessTile.swift
@@ -35,4 +35,37 @@ class ChessTile: Equatable {
         }
     }
     
+    static func getChessTileImage(piece: Character) -> String {
+        switch(piece) {
+            case "p":
+                return "black p"
+            case "P":
+                return "red p"
+            case "r":
+                return "black r"
+            case "R":
+                return "red r"
+            case "n":
+                return "black n"
+            case "N":
+                return "red n"
+            case "b":
+                return "black b"
+            case "B":
+                return "red b"
+            case "k":
+                return "black k"
+            case "K":
+                return "red k"
+            case "q":
+                return "black q"
+            case "Q":
+                return "red q"
+            case " ":
+                return "transparent"
+            default:
+                return "pawn"
+        }
+    }
+    
 }
